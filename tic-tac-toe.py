3c61cf221269c818b2ab8b7a3332bc751e87eced
timot <timothyrosario08@gmail.com>
2022-03-01 16:25:11 -0500

    Add extra indentions

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -3,7 +3,7 @@ board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 class Player:
-  def __init__(self, name, XO):
-    self.name = name
-    self.XO = XO
-    self.win = False
+    def __init__(self, name, XO):
+        self.name = name
+        self.XO = XO
+        self.win = False
