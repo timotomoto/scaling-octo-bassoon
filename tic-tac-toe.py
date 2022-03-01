8e248b051082032a4d6f1ec1e58d7a203eea9a04
timot <timothyrosario08@gmail.com>
2022-03-01 16:23:39 -0500

    Add Player class and constructor with arguments

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,3 +1,9 @@
 import random
 board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+class Player:
+  def __init__(self, name, XO):
+    self.name = name
+    self.XO = XO
+    self.win = False
