b1e0599b9e8d8598421fc1a7ef0258aad3d7e26b
timot <timothyrosario08@gmail.com>
2022-03-01 16:30:15 -0500

    Add description and play method

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -7,3 +7,19 @@ class Player:
         self.name = name
         self.XO = XO
         self.win = False
+
+    def __repr__(self):
+        return "{name} is playing {XO}".format(name=self.name, XO=self.XO)
+
+    def play(self, place):
+        numbers.remove(place)
+        if place <= 2:
+            board[0].pop(place)
+            board[0].insert(place, self.XO)
+        elif place <= 5:
+            board[1].pop(place-3)
+            board[1].insert(place-3, self.XO)
+        elif place <= 8:
+            board[2].pop(place-6)
+            board[2].insert(place-6, self.XO)
+            
